46c5220364594120da40adc825a403b4b1de9fbb
Aman Xd <72649318+releaseuserbot@users.noreply.github.com>
2022-10-07 19:29:56 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -61,7 +61,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/TeleRoid14).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/dailyalltvshows).",
             disable_web_page_preview=True
         )
         return 200
